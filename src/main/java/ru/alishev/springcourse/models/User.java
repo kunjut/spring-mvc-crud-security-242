88008deeee8a5d157799a0bd8b7f39ab7689929d
kunjut <v.dimnich@gmail.com>
2021-03-10 02:40:39 +0300

    Add User model, like POJO

---
+++ src/main/java/ru/alishev/springcourse/models/User.java
@@ -0,0 +1,90 @@
+package ru.alishev.springcourse.models;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+
+    public User() {
+    }
+
+    public User(int id, String name, String surname, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                age == user.age &&
+                name.equals(user.name) &&
+                surname.equals(user.surname) &&
+                email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, age, email);
+    }
+}
