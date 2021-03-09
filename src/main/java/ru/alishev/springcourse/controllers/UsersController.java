85bb7f8e35ff4081dd73169c909d74b89974cbaa
kunjut <v.dimnich@gmail.com>
2021-03-10 02:55:37 +0300

    Add RequestMapping for controller on /users

--- src/main/java/ru/alishev/springcourse/controllers/UsersController.java
+++ src/main/java/ru/alishev/springcourse/controllers/UsersController.java
@@ -1,7 +1,9 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UsersController {
 }
