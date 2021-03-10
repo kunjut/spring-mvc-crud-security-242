a3c298ba28e33789d2e2025bef532bd50123b4ab
kunjut <v.dimnich@gmail.com>
2021-03-10 19:29:40 +0300

    Add GET methods: index(), show() with mapping on URL's

--- src/main/java/ru/alishev/springcourse/controllers/UsersController.java
+++ src/main/java/ru/alishev/springcourse/controllers/UsersController.java
@@ -1,9 +1,30 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
+
+    // GET метод index по адресу /users
+    @GetMapping
+    public String index(Model model) {
+        // строка для отладки
+        System.out.println("в методе index");
+        // из DAO будем получать всех user
+        return null;
+    }
+
+    // GET метод show по адресу /users/:id
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // строка для отладки
+        System.out.println("в методе show");
+        // из DAO будем получать одного user по id
+        return null;
+    }
 }
