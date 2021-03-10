d5d02a890d5b49a0652073b8b11696e62ddf6bdb
kunjut <v.dimnich@gmail.com>
2021-03-10 20:51:56 +0300

    Reformat code

--- src/main/java/ru/alishev/springcourse/dao/UserDAOImp.java
+++ src/main/java/ru/alishev/springcourse/dao/UserDAOImp.java
@@ -9,13 +9,14 @@ public class UserDAOImp implements UserDAO {
     private List<User> userList;
 
     {
-     userList = new ArrayList<>();
-     userList.add(new User(1, "Tyler", "Durden", 25, "b.smith@mail.com"));
-     userList.add(new User(2, "Jeffrey", "Lebowski", 42, "b.johnson@mail.com"));
-     userList.add(new User(3, "Vito", "Corleone", 52, "a.lesly@mail.com"));
-     userList.add(new User(4, "John", "McClane", 38, "i.prince@mail.com"));
-     userList.add(new User(5, "Hans", "Gruber", 40, "b.smith@mail.com"));
+        userList = new ArrayList<>();
+        userList.add(new User(1, "Tyler", "Durden", 25, "b.smith@mail.com"));
+        userList.add(new User(2, "Jeffrey", "Lebowski", 42, "b.johnson@mail.com"));
+        userList.add(new User(3, "Vito", "Corleone", 52, "a.lesly@mail.com"));
+        userList.add(new User(4, "John", "McClane", 38, "i.prince@mail.com"));
+        userList.add(new User(5, "Hans", "Gruber", 40, "b.smith@mail.com"));
     }
+
     @Override
     public List<User> index() {
         return null;
