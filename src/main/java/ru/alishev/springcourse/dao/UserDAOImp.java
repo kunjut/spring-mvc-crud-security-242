79825a80a20b0dd04a23cdeae215daa9f3f5989c
kunjut <v.dimnich@gmail.com>
2021-03-10 19:42:13 +0300

    Add implements DAO

---
+++ src/main/java/ru/alishev/springcourse/dao/UserDAOImp.java
@@ -0,0 +1,17 @@
+package ru.alishev.springcourse.dao;
+
+import ru.alishev.springcourse.models.User;
+
+import java.util.List;
+
+public class UserDAOImp implements UserDAO {
+    @Override
+    public List<User> index() {
+        return null;
+    }
+
+    @Override
+    public User show() {
+        return null;
+    }
+}
