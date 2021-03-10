b47958e6e05c52ca60526f6d33ee91777efe394a
kunjut <v.dimnich@gmail.com>
2021-03-10 21:24:09 +0300

    Add DI userDAO and use it in index()

--- src/main/java/ru/alishev/springcourse/controllers/UsersController.java
+++ src/main/java/ru/alishev/springcourse/controllers/UsersController.java
@@ -1,21 +1,24 @@
 package ru.alishev.springcourse.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.alishev.springcourse.dao.UserDAO;
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
+    @Autowired
+    private UserDAO userDAO;
 
     // GET метод index по адресу /users
     @GetMapping
     public String index(Model model) {
-        // строка для отладки
-        System.out.println("в методе index");
         // из DAO будем получать всех user
+        System.out.println(userDAO.index());
         return null;
     }
 
