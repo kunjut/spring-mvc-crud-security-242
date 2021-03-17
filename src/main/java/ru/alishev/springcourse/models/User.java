86ce4896410b5d38bb81cb3d272cdfd54dd4083f
kunjut <v.dimnich@gmail.com>
2021-03-17 09:59:36 +0300

    Entity User impl UserDetails

--- src/main/java/ru/alishev/springcourse/models/User.java
+++ src/main/java/ru/alishev/springcourse/models/User.java
@@ -1,64 +1,70 @@
 package ru.alishev.springcourse.models;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
-@Table(name="users")
-public class User {
+@Table(name = "users_242")
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
     @Column
-    private String name;
-    @Column
-    private String surname;
-    @Column
-    private Integer age;
+    private String password;
+
     @Column
     private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+//    @JoinTable(name = "user_roles",
+//            joinColumns = @JoinColumn(name = "user_id"),
+//            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 
     public User() {
     }
 
-    public User(int id, String name, String surname, Integer age, String email) {
+    public User(Long id, String username, String password, String email, Set<Role> roles) {
         this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
+        this.username = username;
+        this.password = password;
         this.email = email;
+        this.roles = roles;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getSurname() {
-        return surname;
+    @Override
+    public String getPassword() {
+        return password;
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmail() {
@@ -69,13 +75,20 @@ public class User {
         this.email = email;
     }
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", age=" + age +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
                 '}';
     }
@@ -85,15 +98,39 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return id == user.id &&
-                age.equals(user.age) &&
-                name.equals(user.name) &&
-                surname.equals(user.surname) &&
+        return id.equals(user.id) &&
+                username.equals(user.username) &&
+                password.equals(user.password) &&
                 email.equals(user.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, surname, age, email);
+        return Objects.hash(id, username, password, email);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
