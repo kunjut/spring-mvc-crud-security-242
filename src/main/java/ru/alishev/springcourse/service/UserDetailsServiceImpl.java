4d03c8f4fce8f095129b10225fac58f4fcadbde7
kunjut <v.dimnich@gmail.com>
2021-03-17 09:45:07 +0300

    Impl need for SpringSecurity

---
+++ src/main/java/ru/alishev/springcourse/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package ru.alishev.springcourse.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.alishev.springcourse.dao.UserDAO;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    // «Пользователь» – это просто Object.
+    // В большинстве случаев он может быть приведен к классу UserDetails.
+    // Для создания UserDetails используется интерфейс UserDetailsService, с единственным методом:
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return userDAO.getUserByName(s);
+    }
+}
