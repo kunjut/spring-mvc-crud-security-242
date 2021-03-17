692dbb1472eb86d1376b0d571902c4b3620b544b
kunjut <v.dimnich@gmail.com>
2021-03-17 09:30:47 +0300

    Settings for handler, admin to /admin, other to /user

---
+++ src/main/java/ru/alishev/springcourse/config/handler/LoginSuccessHandler.java
@@ -0,0 +1,34 @@
+package ru.alishev.springcourse.config.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Set;
+
+@Component
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                                        HttpServletResponse httpServletResponse,
+                                        Authentication authentication) throws IOException, ServletException {
+//        httpServletResponse.sendRedirect("/hello");
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        if (roles.contains("ROLE_ADMIN")) {
+            httpServletResponse.sendRedirect("/admin");
+        } else {
+            httpServletResponse.sendRedirect("/user");
+        }
+    }
+}
