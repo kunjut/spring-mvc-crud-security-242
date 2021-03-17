7e1d3014ca7893080d444ef7b5e95fff4215ced1
kunjut <v.dimnich@gmail.com>
2021-03-17 10:12:28 +0300

    revision of save(), update(), call getUserByName

--- src/main/java/ru/alishev/springcourse/service/UserServiceImpl.java
+++ src/main/java/ru/alishev/springcourse/service/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package ru.alishev.springcourse.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.alishev.springcourse.dao.UserDAO;
+import ru.alishev.springcourse.models.Role;
 import ru.alishev.springcourse.models.User;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -13,6 +16,10 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     @Autowired
     UserDAO userDAO;
+    @Autowired
+    UserDetailsServiceImpl userDetailsServiceImpl;
+    @Autowired
+    private PasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public List<User> index() {
@@ -20,22 +27,32 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User show(int id) {
+    public User show(Long id) {
         return userDAO.show(id);
     }
 
     @Override
     public void save(User user) {
+        user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
         userDAO.save(user);
     }
 
     @Override
-    public void update(int id, User updatedUser) {
+    public void update(Long id, User updatedUser) {
+        updatedUser.setPassword(bCryptPasswordEncoder.encode(updatedUser.getPassword()));
+
         userDAO.update(id, updatedUser);
     }
 
     @Override
-    public void delete(int id) {
+    public void delete(Long id) {
         userDAO.delete(id);
+    }
+
+    @Override
+    public User getUserByName(String username) {
+        return userDAO.getUserByName(username);
     }
 }
